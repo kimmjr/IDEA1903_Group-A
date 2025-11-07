d2e1570281f1425b48a4fa687df20d2375c5db1e
lkristinakb <lkle7577@uni.sydney.edu.au>
2025-11-07 15:55:06 +1100

    updates to weave

--- update 02/sketch.js
+++ update 02/sketch.js
@@ -1,7 +1,7 @@
 let img;
 let weaves = [];
-let rowWeaves = 2;
-let colWeaves = 2;
+let rowWeaves = 4;
+let colWeaves = 4;
 let spacing = 12;
 let morphDuration = 150;
 let wormImg;
@@ -33,11 +33,16 @@ function draw() {
   drawWorms(wormImg);
   image(wormImg, 0, 0, width, height);
 
+  push(); 
+  rotate(45);                
+  translate(-width / 2, -height / 2);
 
   for (const weave of weaves) {
     weave.update();
     weave.display();
   }
+  pop();
+
 }
 
 function windowResized() {
